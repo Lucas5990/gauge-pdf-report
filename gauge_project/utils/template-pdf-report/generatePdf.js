c2060e255f66e9ac61dca5a4d0b2f12f6bfdc6a2
Lucas Oliveira Andrade <lolivean@everis.com>
2020-12-16 22:57:54 -0300

    added html-pdf

---
+++ gauge_project/utils/template-pdf-report/generatePdf.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+var pdf = require('html-pdf');
+var html = fs.readFileSync('./test/businesscard.html', 'utf8');
+var options = { format: 'Letter' };
+
+function generatePdf(){
+  pdf.create
+};
